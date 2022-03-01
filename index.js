f47f3318ec10da1a24ad5f659b0d38a46c5ece8a
candacehernandez <chernandez.aggie@gmail.com>
2022-03-01 11:55:08 -0600

    commit tree bcb2811ace7b9de5478e338616a6f0abf43f2651

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        })
+    })
+    .then(response => response.json())
+    .then((data) =>appendID(data))
+    .catch(errorMsg);
+    
+}
+
+function appendID(data){
+    const id = data.id;
+    return document.body.append(id);
+}
+
+function errorMsg(error){
+    alert("Error! Please verify data.");
+    return document.body.append(error.message);
+}
